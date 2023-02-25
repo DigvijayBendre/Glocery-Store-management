29a1a32903e848c19d3eddf24ded698398944fc2
Digvijay Bendre <116354441+DigvijayBendre@users.noreply.github.com>
2023-02-25 12:48:04 +0530

    Add files via upload

---
+++ Code/ui/js/custom/dashboard.js
@@ -0,0 +1,43 @@
+$(function () {
+  //Json data by api call for order table
+  $.get(orderListApiUrl, function (response) {
+    if (response) {
+      var table = "";
+      var totalCost = 0;
+      $.each(response, function (index, order) {
+        totalCost += parseFloat(order.total);
+        table +=
+          '<tr data-id="' +
+          order.order_id +
+          '">' +
+          "<td>" +
+          order.datetime +
+          "</td>" +
+          "<td>" +
+          order.order_id +
+          "</td>" +
+          "<td>" +
+          order.customer_name +
+          "</td>" +
+          "<td>" +
+          order.total.toFixed(2) +
+          " Rs</td>" +
+          '<td><span class="btn btn-xs btn-warning order-details">Details</span></td></tr>';
+      });
+      table +=
+        '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>' +
+        totalCost.toFixed(2) +
+        " Rs</b></td></tr>";
+      $("table").find("tbody").empty().html(table);
+    }
+  });
+});
+
+$(document).on("click", ".order-details", function () {
+  var tr = $(this).closest("tr");
+  var order_id = tr.data("id");
+
+  window.location =
+    "E:/Volume%20II(Data)/Sem%20V/DBMS%20Lab/PReferences/DBMS%20Project/Demorun/ui/order-details.html?order_id=" +
+    order_id;
+});
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Glocery-Store-management
+# Grocery-Store-Management-System
+Implementation of Grocery Store Management System using SQL concepts in Python 3.
+
+The system consists of two main modules:
+1)For Managing Products
+2)For Managing Orders
+
+
+i)In module related to Managing Products functionality is provided for adding new products to 
+the database by providing its name, unit of measurement and price per unit.
+
+ii)Similarly there is an option for deleting an existing product from the database.
+
+iii)In module related to Managing Orders functionality is provided for adding an item in an order 
+by selecting its name from the products list and selecting required number of units. Total price 
+for the selected item is calculated automatically by multiplication of number of units selected 
+and price per unit.
+
+iv)Option for deleting an item is also provided.
+
+v)Also details of an order can be viewed.
+
+
+
